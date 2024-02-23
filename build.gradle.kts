d7c241a66117bde14cd8bd756cfd7eba5696252d
erdiansyah-me <erdiansyah465@gmail.com>
2024-02-23 09:14:10 +0700

    [INITIAL COMMIT] its an Initial Commit with all dependencies and applied modularization

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+    }
+
+}
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+}
