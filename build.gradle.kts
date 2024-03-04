737727055878dd15985cd3f10d23690457b08279
erdiansyah-me <erdiansyah465@gmail.com>
2024-03-04 23:35:50 +0700

    [ADD] Detekt static code analysis [ADD] Jacoco for test coverage [ADD] SQLCipher to encrypt the DB

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        maven(url = "https://jitpack.io%22/")
     }
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
@@ -11,9 +12,9 @@ buildscript {
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
     }
-
 }
 plugins {
+    id("io.gitlab.arturbosch.detekt") version "1.23.5" apply false
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.android.library") version "8.2.2" apply false
--- core/src/main/java/com/greildev/core/app/di/LocalModule.kt
+++ core/src/main/java/com/greildev/core/app/di/LocalModule.kt
@@ -2,6 +2,7 @@ package com.greildev.core.app.di
 
 import android.content.Context
 import androidx.room.Room
+import com.greildev.core.BuildConfig
 import com.greildev.core.data.source.local.database.ErdmoveeDatabase
 import com.greildev.core.utils.CoreConstant
 import dagger.Module
@@ -9,6 +10,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 
 
 @Module
@@ -16,10 +19,13 @@ import dagger.hilt.components.SingletonComponent
 class LocalModule {
     @Provides
     fun provideDatabase(@ApplicationContext context: Context): ErdmoveeDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes(BuildConfig.LIBRARY_PACKAGE_NAME.toCharArray())
+        val factory = SupportFactory(passphrase)
         return Room.databaseBuilder(
             context.applicationContext,
             ErdmoveeDatabase::class.java, CoreConstant.DB_NAME)
             .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
             .build()
     }
 }
